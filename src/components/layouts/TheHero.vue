43c59b2de2150452d1c851b4aa4feadb45f2e295
Stefan Pejcic <pejcha1994@gmail.com>
2022-07-08 22:00:59 +0200

    Hero

--- src/components/layouts/TheHero.vue
+++ src/components/layouts/TheHero.vue
@@ -11,12 +11,20 @@
     >
 
 
-👋 Hi, my name is Stefan Pejčić.<br>
+👋 Hi, my name is Stefan Pejčić.</p>
+<p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
+I'm a <LinkBtn link="https://www.linkedin.com/in/stefan-pejcic/" target="_blank">Linux SysAdmin</LinkBtn> and  <LinkBtn link="https://profiles.wordpress.org/stefanpejcic/#content-plugins" target="_blank">WordPress plugin developer.</LinkBtn>
 
-I'm a <LinkBtn link="https://www.linkedin.com/in/stefan-pejcic/" target="_blank">Linux SysAdmin</LinkBtn> and  <a href="https://profiles.wordpress.org/stefanpejcic/#content-plugins" target="_blank">WordPress plugin developer.</a><br/>
-
-I work as a System Administrator and Technical Support Team Leader at <a href="https://netops.com" target="_blank">NETOPS Group</a> delivering exceptional support to customers through phone, email, and chat. I lead by example, organize work, build reports, handle escalations, and help with ad hoc projects, and training.<br>
-
+</p>
+<p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
+I work as a System Administrator and Technical Support Team Leader at <a href="https://netops.com" target="_blank">NETOPS Group</a> delivering exceptional support to customers through phone, email, and chat. I lead by example, organize work, build reports, handle escalations, and help with ad hoc projects, and training.</p>
+<p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
 When I’m not on the job, I love playing with my dog, spend my time blogging on <a href="https://pcx3.com" target="_blank">PC✗3.com</a> and indulging my love for seeing new places.
     </p>
   </div>
