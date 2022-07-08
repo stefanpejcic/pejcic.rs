7570e5cdad9fc00e01b8c597d96576815ba9830f
stefanpejcic <pejcha1994@gmail.com>
2022-07-08 17:17:48 +0200

    init

---
+++ src/components/layouts/TheHero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col gap-8">
+    <h1
+      class="text-xl sm:text-3xl md:text-4xl md:leading-normal slg:text-5xl slg:leading-relaxed font-theme_bold text-head_text"
+    >
+      Web Developer designing beautiful, intuitive websites<span
+        class="text-link_text_hover"
+        >.</span
+      >
+    </h1>
+    <p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
+      I'm Aniket Kotal, an India based Vue.js developer/Student @ Parul
+      University, with never-stopping love for learning new tech. I am
+      specialising in Web Development with Vue.js, Express, MongoDB and Node.js
+    </p>
+    <p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
+      I'm currently working for
+      <LinkBtn link="https://trileotech.com/">Trileotech</LinkBtn>, a cool web
+      development company in Ahmedabad. I recently worked on
+      <LinkBtn link="https://scarletdynasty.vercel.app">Scarlet Dynasty</LinkBtn
+      >, a website for my friend's team. I am also learning GraphQL and UI/UX.
+    </p>
+  </div>
+</template>
---
+++ src/components/layouts/TheSocials.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-between items-center">
+    <div class="flex gap-1">
+      <SocialIcon
+        title="Github"
+        link="https://github.com/mightykillrr"
+        icon="ant-design:github-filled"
+      />
+      <SocialIcon
+        title="Twitter"
+        link="https://twitter.com/mightykillrr"
+        icon="ant-design:twitter-outlined"
+      />
+      <SocialIcon
+        title="Instagram"
+        link="https://www.instagram.com/aniketkotal/"
+        icon="ant-design:instagram-outlined"
+      />
+    </div>
+    <ActionButton link="mailto:aniket1010104@gmail.com">Email Me</ActionButton>
+  </div>
+</template>
+
+<script>
+import SocialIcon from "../ui/SocialIcon.vue";
+import ActionButton from "../ui/ActionButton.vue";
+
+export default {
+  components: { SocialIcon, ActionButton },
+};
+</script>
