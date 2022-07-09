629c579486beee248d0c4a22465845fe3b8e46d7
stefanpejcic <pejcha1994@gmail.com>
2022-07-09 09:47:55 +0200

    netops

--- src/views/TheAbout.vue
+++ src/views/TheAbout.vue
@@ -3,51 +3,14 @@
     <p
       class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
-      I'm Aniket Kotal, an India based Vue.js developer/Student @ Parul
-      University, with never-stopping love for learning new tech. I am
-      specialising in Web Development with Vue.js, Express, MongoDB and Node.js.
-      I am passionate about the Internet and how we can design beautiful looking
-      interfaces with programming languages.
+      I'm a SysAdmin who dabble with WordPress, crypto and blogging.
+	  Some day it will hopefully all fit together into a unified entity.
     </p>
     <p
       class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
-      I'm currently working on
-      <LinkBtn link="https://scarletdynasty.vercel.app">Scarlet Dynasty</LinkBtn
-      >, a website for my friend's team. I recently worked on
-      <LinkBtn link="https://stust.vercel.app/">Stust</LinkBtn>, a fictional
-      card designer. I am also learning GraphQL and UI/UX. Before this, I just
-      used to design small projects from Frontend Mentor. It has helped me
-      polish my web-development skills.
-    </p>
-    <p
-      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
-    >
-      I started learning about web-development in my early high-school, where I
-      was introduced to HTML and CSS. I quickly started writing small webpages
-      with simple HTML and little of CSS. After my high-school, I took some time
-      to myself and explored the web development world more and found many
-      frameworks. Out of all, I found my love for Vue.js. Since then, I've been
-      mastering it, and with every new project I added a new tool, like
-      TailwindCSS, Express and more.
-    </p>
-    <p
-      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
-    >
-      You can find my projects on my
-      <LinkBtn link="https://github.com/mightykillrr">Github</LinkBtn> or on my
-      <LinkBtn link="/">Portfolio</LinkBtn> page. I usually don't update my
-      portfolio page but all my recent one's are on github.
-    </p>
-    <p
-      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
-    >
-      Outside of my worklife, I love to play Valorant and CS:GO, listening to
-      Em. on Spotify, or just hanging out on Discord with my friends. You can
-      follow along about my life on my
-      <LinkBtn link="https://www.instagram.com/aniketkotal/">Instagram</LinkBtn>
-      or <LinkBtn link="https://twitter.com/mightykillrr">Twitter</LinkBtn>. I
-      post very rarely so you might not find many posts on either.
+      I have over 8 years of experience working in WordPress and cPanel.
+	  Currently, I work as as a System Administrator and Team Leader at NETOPS Group delivering exceptional support to customers through phone, email, and chat. I lead by example, organize work, build reports, handle escalations, and help with ad hoc projects, and training.
     </p>
     <hr class="bg-squgily bg-repeat border-0 h-[6px] opacity-25 my-6" />
     <div class="flex flex-col gap-5">
