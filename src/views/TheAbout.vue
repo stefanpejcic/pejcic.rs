bc947f911c0e034d82bb7a57109c5a02dd87b0b0
stefanpejcic <pejcha1994@gmail.com>
2022-07-12 17:16:53 +0200

    about

--- src/views/TheAbout.vue
+++ src/views/TheAbout.vue
@@ -12,6 +12,22 @@
       I have over 8 years of experience working in <LinkBtn link="https://profiles.wordpress.org/stefanpejcic/#content-plugins" target="_blank">WordPress</LinkBtn> and <LinkBtn link="https://forums.cpanel.net/members/stefanpejcic.1058429/" target="_blank">cPanel</LinkBtn>.
 	  Currently, I work as as a System Administrator and Team Leader at <LinkBtn link="https://netops.com" target="_blank">NETOPS Group</LinkBtn> delivering exceptional support to customers through phone, email, and chat. I lead by example, organize work, build reports, handle escalations, and help with ad hoc projects, and training.
     </p>
+	
+	<p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
+    For the past several years, I’ve been working more closely with cPanel servers on a daily basis providing fully managed cPanel and WordPress support to thousands of customers. I’ve picked up many tricks and written many scripts in this time frame that I want to share with the world.
+    </p>
+	<p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
+    I recently got the chance to write a technical article for my employer and it ignited a fire in my heart. I’ve discovered a new passion for technical writing and this is going to be the focus of my endeavors moving forward.
+    </p>
+	<p
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
+    >
+    I’ve picked up a few tricks with WordPress and other popular web hosting software as well that permeate the hosting landscape in these modern days.
+    </p>
     <hr class="bg-squgily bg-repeat border-0 h-[6px] opacity-25 my-6" />
     <div class="flex flex-col gap-5">
       <h3 class="font-theme_bold text-head_text text-3xl">Work</h3>
