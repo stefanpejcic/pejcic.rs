892865209c9f993a8fa12cca0d6e9222a59c2b15
stefanpejcic <pejcha1994@gmail.com>
2022-07-12 17:09:18 +0200

    wp

--- src/components/layouts/TheHero.vue
+++ src/components/layouts/TheHero.vue
@@ -15,7 +15,7 @@
 <p
       class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
-I am forging my decade plus experience within IT doing: Technical Support, Web Hosting, System Administration, and cPanel Hosting.
+I am forging my decade plus experience within IT doing: Technical Support, Web Hosting, System Administration, and WordPress development.
 </p>
 <p
       class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
@@ -25,7 +25,7 @@ I offer <LinkBtn link="https://pcx3.com" target="_blank">articles</LinkBtn> with
 <p
       class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
-When I’m not on the job, I spend my time blogging and contributing to <router-link class=""
+When I’m not on the job, I spend my time talking about crypto and contributing to <router-link class=""
         to="/projects/#opensource"
       >
         my favorite OpenSource projects.
