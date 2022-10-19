5c8ea0d974ade8e0294a1281a078372fabbd63db
Stefan Pejcic <pejcha1994@gmail.com>
2022-10-19 13:05:52 +0200

    Update TheDomains.vue

--- src/views/TheDomains.vue
+++ src/views/TheDomains.vue
@@ -4,14 +4,13 @@
       <h1
         class="text-2xl sm:text-3xl md:text-4xl slg:text-5xl font-theme_bold text-head_text"
       >
-        Websites<span class="text-link_text_hover">.</span>
+        Premium Domains<span class="text-link_text_hover">.</span>
       </h1>
       <p class="text-base slg:text-lg">
 
-Outside of work, I like doing side projects to try out new tech and build something fun.</p>
+Here are some of my domain names</p>
         
 <p class="text-base slg:text-lg">
-I spend the majority of my free time on these projects.
       </p>
     </div>
     <hr class="bg-squgily bg-repeat border-0 h-[6px] opacity-25" />
@@ -20,24 +19,9 @@ I spend the majority of my free time on these projects.
         v-for="proj in domains"
         :key="proj.id"
         :name="proj.name"
-        :image="proj.image"
         :description="proj.description"
         :link="proj.link"
       />
-    </div>
-    <hr class="bg-squgily bg-repeat border-0 h-[6px] opacity-25" />
-    <div class="flex flex-col gap-2">
-      <h3 id="opensource" class="text-3xl font-theme_bold text-head_text">Open Source</h3>
-      <p>mostly cPanel and WordPress stuff..</p>
-      <div class="flex flex-col mt-10 gap-8">
-        <div v-for="proj in grave" :key="proj.id" class="flex flex-col gap-1">
-          <a class="font-theme_bold text-head_text text-lg" :href="proj.link"
-            >{{ proj.name }}
-            <Icon class="inline text-xl" icon="ep:top-right" color="#eaeaea"
-          /></a>
-          <p class="font-theme text-sm lg:text-lg">{{ proj.description }}</p>
-        </div>
-      </div>
     </div>
   </div>
 </template>
