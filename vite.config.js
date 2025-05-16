541394eb9c8060243cadec5a102981a9532d586d
Stefan Pejcic <pejcha1994@gmail.com>
2025-05-16 17:25:59 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: { host: true },
-  base: "/",
+  base: '/pejcic.rs/',
 });
